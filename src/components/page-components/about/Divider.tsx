4a9e77084d03653d8abab464fc0e427bda1ac5ae
HotPotatoC <juandotulung@gmail.com>
2020-12-24 20:58:34 +0700

    Updates

--- src/components/page-components/about/Divider.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const Divider = () => (
-  <div className="w-full border-b border-white mb-24 md:mb-52"></div>
-)
-
-export default Divider
--- src/gatsby/wrapPageElement.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import Layout from "../components/Layout"
-
-const CustomLayout = ({ element, props }: any, pluginOptions: any) => (
-  <Layout {...props}>{element}</Layout>
-)
-
-export default CustomLayout
